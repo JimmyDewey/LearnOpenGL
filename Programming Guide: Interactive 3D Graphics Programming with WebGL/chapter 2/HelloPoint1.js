f1c2dcc2de87e53a3b62b91f7d83ff1ba217c809
JimmyDewey <fordewey@gmail.com>
2022-01-30 00:12:53 +0800

    Update HelloPoint1.html and HelloPoint1.js

---
+++ Programming Guide: Interactive 3D Graphics Programming with WebGL/chapter 2/HelloPoint1.js
@@ -0,0 +1,26 @@
+let VSHADER_SOURCE =
+    "void main() { \n" +
+    "  gl_Position = vec4(0.0, 0.0, 0.0, 1.0); \n" +
+    "  gl_PointSize = 10.0; \n" +
+    "}\n";
+
+let FSHADER_SOURCE =
+    "void main() { \n" +
+    "  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); \n" +
+    "} \n";
+
+function main() {
+    let canvas = document.getElementById("webgl");
+
+    let gl = getWebGLContext(canvas);
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log("Failed to initialize shaders.");
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
